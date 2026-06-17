ead65c72c96ddc1ac57a9ae98d3a115c4b5c3d4b
sivani12386 <potnurusivani3@gmail.com>
2026-06-17 17:34:32 +0530

    Added search and styling to user list app

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function Users() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     async function getUsers() {
@@ -18,22 +19,35 @@ export default function Users() {
 
     getUsers();
   }, []);
-
+  const filteredUsers = users.filter((user) =>
+  user.name.toLowerCase().includes(search.toLowerCase())
+);
   return (
     <div>
+      <input
+  className="search-box"
+  type="text"
+  placeholder="Search users..."
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+/>
       <h1>👥 User List</h1>
 
       {loading ? (
         <p>Loading users...</p>
       ) : (
-        users.map((user) => (
-          <div key={user.id}>
-            <h2>{user.name}</h2>
-            <p>Email: {user.email}</p>
-            <p>City: {user.address.city}</p>
-            <hr />
-          </div>
-        ))
+        filteredUsers.length > 0 ? (
+  filteredUsers.map((user) => (
+    <div key={user.id} className="user-card">
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+      <p>City: {user.address.city}</p>
+      <hr />
+    </div>
+  ))
+) : (
+  <p>No users found</p>
+)
       )}
     </div>
   );
